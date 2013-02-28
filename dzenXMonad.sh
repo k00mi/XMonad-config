bd2489cb7b48589b8db6570f4d19c391d26ad153
Lukas Braun <koomi@hackerspace-bamberg.de>
2013-02-28 15:12:26 +0100

    Initial commit

---
+++ dzenXMonad.sh
@@ -0,0 +1,36 @@
+#!/bin/zsh
+
+
+bar() {
+    echo $1 | gdbar -fg '#aecf96' -bg '#494b4f' -nonl
+}
+
+mpd() {
+     output=$(mpc | head -2)
+     state=$(echo $output | tail -1 | awk '{print $1 }' | tr -d '[]')
+     case $state in
+       playing) state='▶ ';;
+       paused)  state='▎▎';;
+       *)       state='? ';;
+     esac
+     echo -n "$state"
+     echo -n "$(echo $output | head -1)"
+     echo -n "$(bar $(echo $output | tail -1 | awk '{print $4 }' | tr -d '()%'))"
+}
+
+battery() {
+    state=$(acpi -b | cut -d ' ' -f 3 | tr -d ',')
+    case $state in
+      Discharging) state='<';;
+      Unknown)     state='?';;
+      Charging)    state='>';;
+      Full)        state='F';;
+    esac
+    percentage=$(acpi -b | cut -d "," -f 2 | tr -d " %")
+    echo -n "$status$percentage%"
+}
+
+while true; do
+  echo " $(battery) | $(mpd) | $(date +'%a %B %d, %H:%M')"
+    sleep 3
+done | dzen2 -x 220 -y 0 -h 14 -ta right -fn "-misc-fixed-medium-r-semicondensed-*-12-110-75-75-c-60-koi8-r" -bg "#161616"
---
+++ xmonad.hs
@@ -0,0 +1,180 @@
+-- IMPORTS {{{
+import XMonad
+
+import System.IO
+
+import qualified Data.Map as M
+
+import Graphics.X11.ExtraTypes.XF86
+
+import XMonad.Util.Run
+import XMonad.Util.Dzen
+import XMonad.Util.SpawnOnce
+import XMonad.Actions.Volume
+
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.UrgencyHook
+import XMonad.Hooks.DynamicLog
+
+import XMonad.Layout.Dishes
+import XMonad.Layout.ComboP
+import XMonad.Layout.Tabbed
+import XMonad.Layout.TwoPane
+import XMonad.Layout.Accordion
+import XMonad.Layout.NoBorders
+import XMonad.Layout.PerWorkspace
+-- end of IMPORTS }}}
+
+
+
+-- MAIN {{{
+main :: IO ()
+main = do
+    dzenHandle <- spawnPipe myStatusBar
+    xmonad $ withUrgencyHook dzenUrgencyHook { args = ["-fn", barFont, "-bg", colorDarkCream, "-fg", colorBlue] } $ defaultConfig
+      { modMask             = mod4Mask
+      , terminal            = "urxvt"
+      , workspaces          = map show [1..9]
+      , keys                = \c -> myKeys c `M.union` keys defaultConfig c
+      , layoutHook          = myLayouts
+      , manageHook          = myManageHook <+> manageDocks
+      , startupHook         = myStartupHook
+      , logHook             = myLogHook dzenHandle
+      , normalBorderColor   = colorNormalBorder
+      , focusedBorderColor  = colorFocusedBorder
+      , borderWidth         = 1
+      , focusFollowsMouse   = True
+      }
+-- end of MAIN }}}
+
+
+
+-- KEYS {{{
+myKeys :: XConfig l -> M.Map (ButtonMask, KeySym) (X ())
+myKeys XConfig{XMonad.modMask = modm} = M.fromList
+      [ ((modm,               xK_asciicircum), spawn "slock")
+      , ((modm,               xK_Return     ), spawn "urxvt")
+      , ((modm,               xK_p          ), spawn "dmenu_run -b")
+
+      , ((modm,               xK_Up         ), spawn "mpc -q next")
+      , ((modm,               xK_Down       ), spawn "mpc -q prev")
+      , ((modm,               xK_c          ), spawn "mpc -q toggle")
+      , ((modm .|. shiftMask, xK_m          ), spawn "/home/koomi/bin/mpdmenu")
+
+      , ((0,         xF86XK_AudioRaiseVolume), raiseVolumeChannels ["Master"] 2 >>= alert)
+      , ((0,         xF86XK_AudioLowerVolume), lowerVolumeChannels ["Master"] 2 >>= alert)
+      ]
+-- end of KEYS }}}
+
+
+
+-- HOOKS {{{
+myStartupHook = composeAll
+    [ spawn     "/home/koomi/.xmonad/dzenXMonad.sh"
+    , spawnOnce "dwb"
+    , spawnOnce "pidgin"
+    , spawnOnce "urxvt -e 'weechat-curses'"
+    ]
+
+
+myManageHook :: ManageHook
+myManageHook = composeAll
+    [ appName     =?  "weechat 0.4.0" --> doShift "2"
+    , className   =?  "Pidgin"        --> doShift "2"
+    , className   =?  "Dwb"           --> doShift "1"
+    , className   =?  "mplayer2"      --> doFloat
+    , className   =?  "sxiv"          --> doFullFloat
+    , className   =?  "Steam"         --> doFullFloat
+    ]
+-- end of HOOKS }}}
+
+
+
+-- DZEN {{{
+myStatusBar = "dzen2 -x '0' -y '0' -h '14' -w '220' -ta 'l' -bg '" ++ colorDarkGray ++ "' -fg '" ++ colorBlue ++ "' -fn '" ++ barFont ++ "'"
+
+myLogHook :: Handle -> X ()
+myLogHook h = dynamicLogWithPP $ defaultPP
+    {
+        ppCurrent           =   dzenColor colorDarkGray   colorCream    . hideScratchpad
+      , ppVisible           =   dzenColor colorCream      colorDarkGray . hideScratchpad
+      , ppHidden            =   dzenColor colorBlue       colorDarkGray . hideScratchpad
+      , ppHiddenNoWindows   =   dzenColor colorGray       colorDarkGray . hideScratchpad
+      , ppUrgent            =   dzenColor colorMagenta    colorDarkGray . pad
+      , ppWsSep             =   ""
+      , ppSep               =   " | "
+      , ppTitle             =   const ""
+      , ppOutput            =   hPutStrLn h
+    }
+    where
+      hideScratchpad ws = if ws == "NSP" then "" else pad ws -- hide sp in ws list (thanks to p.brisbin)
+
+
+alert = dzenConfig conf . show . round
+  where
+    conf = timeout 2
+       >=> onCurr (center 20 14)
+       >=> font barFont
+       >=> addArgs ["-bg", colorDarkGray]
+       >=> addArgs ["-fg", colorBlue]
+-- end of DZEN }}}
+
+
+
+-- COLORS, FONTS, AND PROMPTS {{{
+
+-- <colors>
+colorBlack          = "#000000"
+colorBlackAlt       = "#040404"
+colorGray           = "#444444"
+colorGrayAlt        = "#282828"
+colorDarkGray       = "#161616"
+colorWhite          = "#cfbfad"
+colorWhiteAlt       = "#8c8b8e"
+colorDarkWhite      = "#606060"
+colorCream          = "#a9a6af"
+colorDarkCream      = "#5f656b"
+colorMagenta        = "#a488d9"
+colorMagentaAlt     = "#7965ac"
+colorDarkMagenta    = "#8e82a2"
+colorBlue           = "#98a7b6"
+colorBlueAlt        = "#598691"
+colorDarkBlue       = "#464a4a"
+colorNormalBorder   = colorGray
+colorFocusedBorder  = colorBlue
+
+-- <font>
+barFont = "-misc-fixed-medium-r-semicondensed-*-12-110-75-75-c-60-koi8-r"
+
+-- <tab-bar configuration>
+myTabTheme =
+    defaultTheme { fontName            = barFont
+                 , inactiveBorderColor = colorGrayAlt
+                 , inactiveColor       = colorDarkGray
+                 , inactiveTextColor   = colorGrayAlt
+                 , activeBorderColor   = colorGrayAlt
+                 , activeColor         = colorDarkMagenta
+                 , activeTextColor     = colorDarkGray
+                 , urgentBorderColor   = colorBlackAlt
+                 , urgentTextColor     = colorWhite
+                 , decoHeight          = 14
+                 }
+-- end of COLORS, FONTS, AND PROMPTS }}}
+
+
+
+-- LAYOUTS {{{
+myLayouts = smartBorders $ avoidStruts
+    $ onWorkspace "2" chatLayout
+    $ tall ||| Mirror tall ||| Full ||| Accordion ||| Dishes 2 (1/6)
+  where
+    tall = Tall 1 (3/100) (1/2)
+    chatLayout = combineTwoP (Mirror (TwoPane (3/100) (1/2)))
+                              (tabbed shrinkText myTabTheme)
+                              Full
+                             (ClassName "Pidgin")
+-- end of LAYOUTS }}}
+
+
+-- vim:foldmethod=marker foldmarker={{{,}}} sw=2 sts=2 ts=2 tw=0 et ai nowrap
